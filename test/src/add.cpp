ff92d606a112b7e430433ca7d8ac799769603e62
Songjiaxin11 <1003414719@qq.com>
2023-09-19 17:30:01 +0800

    change settings in .vscode, don't change it again. The current version is in vscode_settings.

---
+++ test/src/add.cpp
@@ -0,0 +1,8 @@
+#include "../include/add.h"
+#include <iostream>
+using namespace std;
+int add (int a,int b)
+{
+    cout<<a+b<<endl;
+    return a+b;
+}
---
+++ test/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "../include/add.h"
+using namespace std;
+int main()
+{
+    // int a;
+    add(1, 2);
+    cout << "it works" << endl;
+    return 0;
+}
